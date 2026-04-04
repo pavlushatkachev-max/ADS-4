4fe262bb318697f1c50c611aab8758ffe9565a42
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-04-04 23:41:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,13 @@ int countPairs1(int *arr, int len, int value) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
+                while (j + 1 < len && arr[j + 1] == arr[j]) {
+                    j++;
+                }
             }
+        }
+        while (i + 1 < len && arr[i + 1] == arr[i]) {
+            i++;
         }
     }
     return count;
