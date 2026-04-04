f45f0b60005547d75d008b2823d7e061ba10f2ff
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-04-04 23:37:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,18 +18,20 @@ int countPairs2(int *arr, int len, int value) {
     while (left < right) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
-            if (arr[left] == arr[right]) {
+            int leftVal = arr[left];
+            int rightVal = arr[right];
+            if (leftVal == rightVal) {
                 int n = right - left + 1;
                 count += n * (n - 1) / 2;
                 break;
             } else {
                 int leftCount = 1;
                 int rightCount = 1;
-                while (left + 1 < right && arr[left + 1] == arr[left]) {
+                while (left + 1 < right && arr[left + 1] == leftVal) {
                     leftCount++;
                     left++;
                 }
-                while (right - 1 > left && arr[right - 1] == arr[right]) {
+                while (right - 1 > left && arr[right - 1] == rightVal) {
                     rightCount++;
                     right--;
                 }
@@ -60,31 +62,12 @@ int binarySearch(int *arr, int left, int right, int target) {
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int i = 0;
-    while (i < len) {
-        int currentVal = arr[i];
-        int currentCount = 1;
-        while (i + 1 < len && arr[i + 1] == currentVal) {
-            currentCount++;
-            i++;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int found = binarySearch(arr, i + 1, len - 1, target);
+        if (found != -1) {
+            count++;
         }
-        int target = value - currentVal;
-        if (target > currentVal) {
-            int pos = binarySearch(arr, i + 1, len - 1, target);
-            if (pos != -1) {
-                int targetCount = 1;
-                int j = pos;
-                while (j + 1 < len && arr[j + 1] == target) {
-                    targetCount++;
-                    j++;
-                }
-                count += currentCount * targetCount;
-            }
-        } else if (target == currentVal) {
-            count += currentCount * (currentCount - 1) / 2;
-            break;
-        }
-        i++;
     }
     return count;
 }
