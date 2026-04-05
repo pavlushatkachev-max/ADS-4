4883ab63a117e414bf4461f1ba3f298081691253
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-04-05 23:57:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,73 +1,72 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value)
-                  count++;
-        }
-    }
-    return count;
+#include <iostream>
+
+int countPairs1(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len / 2 + 1; i++)
+    for (int j = i+1; j < len; j++)
+      if (arr[i] + arr[j] == value)
+        count += 1;
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-    int left = 0;
-    int right = len - 1;
-    int count = 0;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int k = right - left + 1;
-                count += k * (k - 1) / 2;
-                break;
-            } else {
-                }
-                int l_val = arr[left], r_val = arr[right];
-                int l_count = 0, r_count = 0;
-                while (left < len && arr[left] == l_val) {
-                    l_count++;
-                    left++;
-                }
-                while (right >= 0 && arr[right] == r_val) {
-                    r_count++;
-                    right--;
-                }
-                count += l_count * r_count;
-            }
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-    return count;
-}
-int binarySearch(int *arr, int left, int right, int target) {
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == target)
-            return mid;
-        else if (arr[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+int binSearch(int* arr, int value, int l, int r) {
+  if (l > r)
     return -1;
+  else if (arr[(r + l) / 2] == value)
+    return (r + l) / 2;
+  else if (arr[(r + l) / 2] > value)
+    return binSearch(arr, value, l, (r + l) / 2 - 1);
+  else
+    return binSearch(arr, value, (r + l) / 2 + 1, r);
 }
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        int target = value - arr[i];
-        int idx = binarySearch(arr, i + 1, len - 1, target);
-        if (idx != -1) {
-            int left = idx;
-            int right = idx;
-            while (left > i && arr[left - 1] == target)
-                left--;
-            while (right < len - 1 && arr[right + 1] == target)
-                right++;
-            count += (right - left + 1);
-        }
+int countPairs2(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len / 2 + 1; i++) {
+    int chislo = value - arr[i];
+    int bin = binSearch(arr, chislo, i + 1, len - 1);
+    if (bin != -1) {
+      int l = bin;
+      while (l > i && arr[i] + arr[l] == value) {
+        count += 1;
+        l -= 1;
+      }
+      int r = bin + 1;
+      while (r < len && arr[i] + arr[r] == value) {
+        count += 1;
+        r += 1;
+      }
     }
-    return count;
+  }
+  return count;
+}
+int countPairs3(int* arr, int len, int value) {
+  int count = 0,
+    i = 0,
+    j = len - 1;
+  while (i < j) {
+    if (arr[i] + arr[j] < value) {
+       i++;
+    } else if (arr[i] + arr[j] > value) {
+      j--;
+    } else {
+      int l = 1, r = 1;
+      if (arr[i] == arr[j]) {
+        int n = j - i + 1;
+        count += n * (n - 1) / 2;
+        break;
+      }
+      while (arr[i] == arr[i + 1]) {
+        l++;
+        i++;
+      }
+      while (arr[j] == arr[j - 1]) {
+        r++;
+        j--;
+      }
+      count += r * l;
+      i++;
+      j--;
+    }
+  }
+  return count;
 }
